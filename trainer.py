9b753133a696ea9ed2636dc0eaae78faa9987af3
fadilparves <fadil.parves@gmail.com>
2020-11-21 12:12:35 +0800

    train init ready

--- trainer.py
+++ trainer.py
@@ -17,3 +17,36 @@ from torch.utils.data import DataLoader
 
 from .torch_dataset import TorchDataset
 
+class MaskDetectorTrainer(pl.LightningModule):
+    def __init__(self, mask_df_path: Path=None):
+        super(MaskDetectorTrainer, self).__init__()
+        self.mask_df_path = mask_df_path
+
+        self.conv_layer_1 = conv_layer_1 = Sequential(
+            Conv2d(3, 32, kernel_size=(3,3), padding=(1,1)),
+            ReLU(),
+            MaxPool2d(kernel_size=(2,2))
+        )
+
+        self conv_layer_2 = conv_layer_2 = Sequential(
+            Conv2d(32, 64, kernel_size=(3,3), padding=(1,1)),
+            ReLU(),
+            MaxPool2d(kernel_size=(2,2))
+        )
+
+        self.conv_layer_3 = conv_layer_3 = Sequential(
+            Conv2d(64, 128, kernel_size=(3,3), padding=(1,1), stride=(3,3)),
+            ReLU(),
+            MaxPool2d(kernel_size=(2,2))
+        )
+
+        self.linear_layer = linear_layer = Sequential(
+            Linear(in_features=2048, out_features=1024),
+            ReLU(),
+            Linear(in_features=1024, out_features=2),
+        )
+
+        for sequential in [conv_layer_1, conv_layer_2, conv_layer_3, linear_layer]:
+            for layer in sequential.children():
+                if isinstance(layer, (Linear, Conv2d)):
+                    init.xavier_uniform_(layer.weight)
