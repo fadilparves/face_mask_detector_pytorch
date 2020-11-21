68e6b7bde72382fabc9fe98b82cfaffc7195ab2a
fadilparves <fadil.parves@gmail.com>
2020-11-21 12:21:18 +0800

    prepare data with custom weight ready

--- trainer.py
+++ trainer.py
@@ -58,3 +58,15 @@ class MaskDetectorTrainer(pl.LightningModule):
         out = out.view(-1, 2048)
         out = self.linear_layer(out)
         return out
+
+    def prepare_data(self) -> None:
+        self.mask_df = mask_df = pd.read_pickle(self.mask_df_path)
+        train, validate = train_test_split(mask_df, test_size=0.3, random_state=0, stratify=mask_df['mask'])
+        self.train_df = TorchDataset(train)
+        self.validate_df = TorchDataset(validate)
+
+        mask_num = mask_df[mask_df['mask']==1].shape[0]
+        non_mask_num = mask_df[mask_df['mask']==0].shape[0]
+        samples = [non_mask_num, mask_num]
+        norm_weights = [1 - (x / sum(samples)) for x in samples]
+        self.cross_entropy_loss = CrossEntropyLoss(weight=torch.tensor(norm_weights))
