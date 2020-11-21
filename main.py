c27f3784c5b093e8a9649a597e10884f2540477e
fadilparves <fadil.parves@gmail.com>
2020-11-21 15:10:59 +0800

    testing cv2

--- main.py
+++ main.py
@@ -11,3 +11,26 @@ net = cv2.dnn.readNetFromCaffe('./checkpoints/deploy.prototxt.txt', './checkpoin
 print("[INFO] Starting stream")
 vs = VideoStream(src=0).start()
 time.sleep(2)
+
+while True:
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+
+    (h,w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300,300)), 1.0, (300,300), (104.0, 177.0, 123.0))
+
+    net.setInput(blob)
+    detections = net.forward()
+
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0,0,i,2]
+        if confidence < 0.6:
+            continue
+
+        box = detections[0,0,i,3:7] * np.array([w,h,w,h])
+        (startX, startY, endX, endY) = box.astype("int")
+
+        text = "{:.2f}%".format(confidence * 100)
+        y = startY - 10 if startY - 10 > 10 else startY + 10
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+        cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0 , 0, 255), 2)
