b3746aba06ede8ea1ae7b8e388c571858683e147
fadilparves <fadil.parves@gmail.com>
2020-11-21 15:30:13 +0800

    working on real pred

--- main.py
+++ main.py
@@ -12,39 +12,50 @@ from face_detector import FaceDetector
 from trainer import MaskDetectorTrainer
 
 print("[INFO] Loading model")
-net = cv2.dnn.readNetFromCaffe('./checkpoints/deploy.prototxt.txt', './checkpoints/res10_300x300_ssd_iter_140000.caffemodel')
+fd_model = FaceDetector(
+        prototype='./checkpoints/deploy.prototxt.txt',
+        model='./checkpoints/res10_300x300_ssd_iter_140000.caffemodel',
+    )
 
-print("[INFO] Starting stream")
-vs = VideoStream(src=0).start()
-time.sleep(2)
+model = MaskDetectorTrainer()
+model.load_state_dict(torch.load('./checkpoints/weights-v0.ckpt')['state_dict'], strict=False)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+model.eval()
 
-while True:
-    frame = vs.read()
-    frame = imutils.resize(frame, width=400)
+@torch.no_grad()
+def tagMask(model, fd_model, source, outputPath):
+    print("[INFO] Starting stream")
+    vs = VideoStream(src=0).start()
+    time.sleep(2)
 
-    (h,w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300,300)), 1.0, (300,300), (104.0, 177.0, 123.0))
+    while True:
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
 
-    net.setInput(blob)
-    detections = net.forward()
+        (h,w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300,300)), 1.0, (300,300), (104.0, 177.0, 123.0))
 
-    for i in range(0, detections.shape[2]):
-        confidence = detections[0,0,i,2]
-        if confidence < 0.6:
-            continue
+        net.setInput(blob)
+        detections = net.forward()
 
-        box = detections[0,0,i,3:7] * np.array([w,h,w,h])
-        (startX, startY, endX, endY) = box.astype("int")
+        for i in range(0, detections.shape[2]):
+            confidence = detections[0,0,i,2]
+            if confidence < 0.6:
+                continue
 
-        text = "{:.2f}%".format(confidence * 100)
-        y = startY - 10 if startY - 10 > 10 else startY + 10
-        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
-        cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0 , 0, 255), 2)
+            box = detections[0,0,i,3:7] * np.array([w,h,w,h])
+            (startX, startY, endX, endY) = box.astype("int")
 
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
+            text = "{:.2f}%".format(confidence * 100)
+            y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+            cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0 , 0, 255), 2)
 
-cv2.destroyAllWindows()
-vs.stop()
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
+    vs.stop()
